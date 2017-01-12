b50c145099cfe26e5507e253b0078c86cffb9ed8
Fred Wang <namelos@outlook.com>
2017-01-12 23:15:13 +0800

    add react tests

--- web/static/js/test/App.test.js
+++ web/static/js/test/App.test.js
@@ -1,4 +1,19 @@
 import test from 'ava'
+import React from 'react'
+import {renderJSX, JSX} from 'jsx-test-helpers'
+import {shallow} from 'enzyme'
+
+import App from '../App'
 
 test('truth', t => t.true(true))
 
+test('App render to string', t => {
+  const actual = renderJSX(<App />)
+  const expected = JSX(<div>Hello world!</div>)
+  t.is(actual, expected)
+})
+
+test('App shallow render', t => {
+  const wrapper = shallow(<App />)
+  t.true(wrapper.contains(<div>Hello world!</div>))
+})
