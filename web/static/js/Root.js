f4c96b92f37ec78e9b5edff45d0cb4e8e816ac2a
Fred Wang <namelos@outlook.com>
2017-01-14 00:33:04 +0800

    finish hot reload for redux and react router

--- web/static/js/Root.js
+++ web/static/js/Root.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { Router, browserHistory } from 'react-router'
 import { reducer } from './model'
-import { CounterApp } from './view'
+import { routes } from './routes'
 
 const store = createStore(reducer)
 
 export default () => <Provider store={store}>
-  <CounterApp />
+  <Router history={browserHistory} routes={routes} />
 </Provider>
--- web/static/js/view/index.js
+++ web/static/js/view/index.js
@@ -1,1 +1,2 @@
 export CounterApp, { Counter } from './Counter'
+export { App } from './App'
