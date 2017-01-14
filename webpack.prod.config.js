7e2936a7d646f669ce5f53a719739ef5c2e541ad
Fred Wang <namelos@outlook.com>
2017-01-14 15:09:54 +0800

    add prodution webpack config

---
+++ webpack.prod.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './web/static/js/index.js',
+  output: {
+    path: path.join(__dirname, 'priv/static/js'),
+    filename: 'app.min.js',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+        screw_ie8: true
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      include: path.join(__dirname, 'web/static/js')
+    }]
+  }
+}
